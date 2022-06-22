5ec1f5eacb23781b5aa0eaf780bc13037c420c7f
Adi Purnomo <dev.adipurnomo@gmail.com>
2022-06-22 17:06:55 +0700

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,10 @@
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
+
+sudo apt install dirmngr gnupg apt-transport-https software-properties-common ca-certificates curl -y
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
